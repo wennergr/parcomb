d0da2ae5d06260d1972335ebff0e8f11c33de534
Tobias Wennergren <wennergr@gmail.com>
2023-01-17 20:43:21 -0800

    Negative number support for integer parser

--- parcomb/number.py
+++ parcomb/number.py
@@ -1,9 +1,11 @@
 from .parsing import Parser
-from .char import satisfy
-from .combinator import many1
+from .char import satisfy, char
+from .combinator import many1, option
 
 
 def integer() -> Parser[int]:
     isdigit = satisfy(lambda c: c.isdigit(), f"#integer: Failed to parse integer")
 
-    return many1(isdigit).map(lambda x: int("".join(x)))
+    return option("", char("-")).and_then(
+        lambda s: many1(isdigit).map(lambda x: int(s + "".join(x)))
+    )
--- tests/test_number.py
+++ tests/test_number.py
@@ -4,5 +4,7 @@ from parcomb.number import *
 
 def test_integer():
     assert_success(integer().run("12345"), 12345, "")
+    assert_success(integer().run("-12345"), -12345, "")
     assert_success(integer().run("12abc"), 12, "abc")
+    assert_success(integer().run("-12abc"), -12, "abc")
     assert_failure(integer().run("abc12abc"), "abc12abc")
